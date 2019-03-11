93772af1b345d26a652896ac496283af30d7b877
Anomali007 <franzese007@gmail.com>
2019-03-11 10:56:38 -0700

    Write to file with generateData.js makes json file. Handle CRUD routes in server controller

---
+++ generateData.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const file = fs.createWriteStream('./exploresData.json');
+
+// All mock data
+const { generator } = require('./db/dataFormat.js');
+
+
+function writeOneMillionTimes(writer, data, encoding, callback) {
+  let i = 0;
+  write();
+  function write() {
+    let ok = true;
+    do {
+      i++;
+      console.log(i);
+      let index = i;
+      // adjustedData[i % 600]["myid"] = index;
+
+      let newData = generator(index);
+
+      // let dataString = JSON.stringify(adjustedData[i % 600]) + '\n';
+      let dataString = JSON.stringify(newData) + '\n';
+
+
+      if (i === 10000000) {
+        // last time!
+        file.write(dataString, encoding, callback);
+      } else {
+        // see if we should continue, or wait
+        // don't pass the callback, because we're not done yet.
+        // data = JSON.stringify(data);
+        ok = file.write(dataString, encoding);
+      }
+    } while (i < 10000000 && ok);
+    if (i < 10000000) {
+      // had to stop early!
+      // write some more once it drains
+      file.once('drain', write);
+    }
+  }
+}
+
+writeOneMillionTimes(file, '', "utf8", (err) => {
+  if (err) {
+    console.log('error writing', err);
+  } else {
+    console.log('Successful going');
+    file.end();
+  }
+})
+
+
+
+
+// **************************************** Terminal Commands ****************************************
+//mongoimport --db exploreProducts --collection explores --file exploresData.txt
+//mongoimport --db exploreProducts --collection explores --file exploresData.json
+
+// db.explores.find({productId: 99992299}).explain("executionStats")
+
+// Hash index created on productId
+//  db.explores.createIndex( { productId: "hashed" } )
--- server/controller.js
+++ server/controller.js
@@ -1,17 +1,66 @@
-const db = require('../db/index.js');
+// const { fetchExplore } = require('../db/dbHelpers.js');
+const Explores = require('../db/model.js');
 
 module.exports = {
-  explores: {
-      get: (req, res) => {
-        let { id } = req.params;
-        db.fetchExplore(id, (err, data) => {
-              if (err) {
-                  res.status(404).send()
-              } else {
-                  res.status(200).send(data);
-              }
-          })
-      }
-  }
+    explores: {
+        get: (req, res) => {
+            let { id } = req.params;
 
-}
+            Explores.find({ productId: id })
+                .then((data) => {
+                    console.log('success in fetchExplore');
+                    res.status(200).send(data);
+                })
+                .catch((err) => {
+                    console.log('failure in fetchExplore');
+                    res.status(404).send();
+                });
+        },
+        post: (req, res) => {
+            let { id } = req.params;
+
+            let explores = new Explores(
+                {
+                    productId: id,
+                    exploresLists: req.body.explores,
+                    videosLists: req.body.videos,
+                    articlesLists: req.body.articles,
+                    innerCarousel: req.body.carousel
+                }
+            );
+            explores.save()
+                .then(() => {
+                    res.status(201).send('Product Created.');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(404).end();
+                })
+
+
+        },
+        update: (req, res) => {
+            let { id } = req.params;
+            let newId = req.body;
+            Explores.findByIdAndUpdate(id, newId)
+                .then((product) => {
+                    res.status(201).send('Product udpated.');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(404).end();
+                })
+        },
+        delete: (req, res) => {
+            let { id } = req.params;
+            Explores.findByIdAndRemove(id)
+                .then(() => {
+                    res.status(202).send('Delete successful.');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(404).end();
+                })
+        }
+    }
+};
