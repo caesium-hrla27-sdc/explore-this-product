bbb23bf60553e38776cdf9a92c4f831980c59131
Anomali007 <franzese007@gmail.com>
2019-03-15 11:43:37 -0700

    Added import file to import all of the generated csv data into the postgresql db

---
+++ importFile.sql
@@ -0,0 +1,5 @@
+COPY generator FROM '/Users/Mali/Desktop/HackReactor/SDC/explore-this-product/generator.csv' DELIMITER ',' CSV HEADER;
+COPY explorer FROM '/Users/Mali/Desktop/HackReactor/SDC/explore-this-product/explorer.csv' DELIMITER ',' CSV HEADER;
+COPY articles FROM '/Users/Mali/Desktop/HackReactor/SDC/explore-this-product/articles.csv' DELIMITER ',' CSV HEADER;
+COPY videos FROM '/Users/Mali/Desktop/HackReactor/SDC/explore-this-product/videos.csv' DELIMITER ',' CSV HEADER;
+COPY inner_carousel FROM '/Users/Mali/Desktop/HackReactor/SDC/explore-this-product/inner_carousel.csv' DELIMITER ',' CSV HEADER;
