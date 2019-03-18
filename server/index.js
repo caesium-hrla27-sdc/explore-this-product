eda084fa5386815984a6ef09e17959e4fd33db14
Anomali007 <franzese007@gmail.com>
2019-03-18 14:08:09 -0700

    Updated server to verify loader.io

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parser = require('body-parser');
 const morgan = require('morgan');
 const controller = require('./mongo/controller.js');
-const PORT = 3005;
+const PORT = 4444;
 
 const db = require('../db/mongo/index.js');
 
@@ -20,4 +20,12 @@ app.post('/explores/:id', controller.explores.post);
 app.put('/explores/:id', controller.explores.update);
 app.delete('/explores/:id', controller.explores.delete);
 
+// Loader.io verififcation route
+app.get('/loaderio-4149a8bff042e152ce3a6f8106e29585.txt', (req, res) => {
+  res.status(200).send('loaderio-4149a8bff042e152ce3a6f8106e29585')
+})
+
 app.listen(PORT, () => console.log('the server is listening on ', PORT));
+
+
+// loaderio-4149a8bff042e152ce3a6f8106e29585
